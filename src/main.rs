42d8b3c65557a5771079019d05f8ae21467b4cda
Rat Girl <chloeholmes8192@gmail.com>
2020-11-25 15:12:34 +1100

    Lookup other people's points

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@ use std::sync::{Arc, RwLock};
 use std::sync::atomic::Ordering;
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufRead, BufWriter, Write};
-use serenity::model::id::RoleId;
+use serenity::model::id::{RoleId, UserId};
+use serenity::static_assertions::_core::str::FromStr;
+use serenity::model::user::User;
 
 #[group]
 #[commands(getpoints, givepoints)]
@@ -52,10 +54,36 @@ async fn main() {
 
 #[command]
 async fn getpoints(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Your points").await?;
-    let user_id = msg.author.id;
+    let mut user_id = msg.author.id.to_string();
 
-    let mut scores: HashMap<String, i32> = HashMap::new();
+    //get args
+    let mut content = msg.content.to_string();
+    content.remove(0);
+    let sections: Vec<&str> = content.split_ascii_whitespace().collect();
+    if(sections.len() > 1) {
+        user_id = sections[1].to_string();
+    }
+
+    let user = UserId::from_str(&user_id).unwrap_or(UserId(333)).to_user(ctx).await;
+    let username: String;
+    match user {
+        Ok(u) => {username = u.name}
+        _ => {
+            msg.channel_id.send_message(&ctx, |m| {
+                m.content("");
+                m.embed(|mut e| {
+                    e.title("Point Count");
+                    e.description("Could not find user");
+                    e.color(0x6e10aau64);
+                    e
+                });
+                m
+            }).await?;
+            return Ok(());
+        }
+    }
+
+    let mut scores: HashMap<String, i64> = HashMap::new();
 
     {
         let file = File::open(path).expect("Can't find the file");
@@ -64,16 +92,37 @@ async fn getpoints(ctx: &Context, msg: &Message) -> CommandResult {
             let ln: String = line.unwrap();
             let tokens: Vec<&str> = ln.split(":").collect();
 
-            scores.insert(tokens[0].to_string(), i32::from(tokens[1].to_string().parse::<i32>().unwrap()));
+            scores.insert(tokens[0].to_string(), i64::from(tokens[1].to_string().parse::<i64>().unwrap()));
         }
     }
 
     if !scores.contains_key(user_id.to_string().as_str()) {
-        msg.reply(ctx, "You don't have any points").await?;
+        msg.channel_id.send_message(&ctx, |m| {
+            m.content("");
+            m.embed(|mut e| {
+                e.title("Point Count");
+                e.description("User ".to_owned() + &username + " doesn't have any points");
+
+                e
+            });
+            m
+        }).await?;
+
     } else {
         let curScore = *scores.get(&user_id.to_string()).unwrap();
-        let message : String = "You have ".to_owned() + &*curScore.to_string() + " points";
-        msg.reply(ctx, message).await?;
+        let message : String = username.as_str().to_owned() + " has " + &*curScore.to_string() + " points";
+
+        msg.channel_id.send_message(&ctx, |m| {
+            m.content("");
+            m.embed(|mut e| {
+                e.title("Point Count");
+                e.description(message);
+
+                e
+            });
+            m
+        }).await?;
+
         scores.insert(user_id.to_string(), curScore + 1);
     }
 
@@ -98,11 +147,11 @@ async fn givepoints(ctx: &Context, msg: &Message) -> CommandResult {
     content.remove(0);
     let sections: Vec<&str> = content.split_ascii_whitespace().collect();
     let user_id = sections[1].to_string();
-    let amt = sections[2].parse::<i32>().unwrap();
+    let amt = sections[2].parse::<i64>().unwrap();
 
 
     println!("Command");
-    let mut scores: HashMap<String, i32> = HashMap::new();
+    let mut scores: HashMap<String, i64> = HashMap::new();
 
     {
         let file = File::open(path).expect("Can't find the file");
@@ -111,7 +160,7 @@ async fn givepoints(ctx: &Context, msg: &Message) -> CommandResult {
             let ln: String = line.unwrap();
             let tokens: Vec<&str> = ln.split(":").collect();
 
-            scores.insert(tokens[0].to_string(), i32::from(tokens[1].to_string().parse::<i32>().unwrap()));
+            scores.insert(tokens[0].to_string(), i64::from(tokens[1].to_string().parse::<i64>().unwrap()));
         }
     }
 
